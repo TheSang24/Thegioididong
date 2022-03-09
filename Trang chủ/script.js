6ed6f859358d0593ef79ce363e8ebe0613bb400b
TheSang24 <bavuongsang2002@gmail.com>
2022-03-09 20:56:21 +0700

    trangchu

---
+++ Trang chủ/script.js
@@ -0,0 +1,9 @@
+const adressbtn = document.querySelector('#adress-form')
+addressbtn.addEventListener("click", function(){
+    document.querySelector('.adress-form').getElementsByClassName.display = "flex"
+})
+
+// const rightbtn = document.querySelector('.fa-chevron-right')
+// rightbtn.addEventListener ("click", function(){
+//     console.log (ok) 
+// })
